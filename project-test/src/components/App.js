6f57c5352466fedddc974d4413804c4b666d0e94
yakshina <yakshina_kristina@mail.ru>
2021-10-17 00:33:01 +0300

    refactor: settings

---
+++ project-test/src/components/App.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+import '../styles/App.css';
+ 
+class App extends Component{
+  render(){
+    return (
+      <div>
+         <h1>Hello, react.js!!!</h1>
+      </div>
+    );
+  }
+}
+ 
+export default App;
---
+++ project-test/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(sass|scss)$/,
+        use: [{
+            loader: "style-loader" // creates style nodes from JS strings
+        }, {
+            loader: "css-loader" // translates CSS into CommonJS
+        }, {
+            loader: "sass-loader" // compiles Sass to CSS
+        }]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ]
+};
